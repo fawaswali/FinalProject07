073151913145f15a6ca1888aba5c15909b4f6b62
Naufalentino <naufalvalentino422@gmail.com>
2025-10-01 13:44:45 +0700

    commit by nopal

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Ini collab eak");
+        System.out.println("Ini nopal vroh");
     }
 }
